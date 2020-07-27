c930cb8a808ac7d487a1dc66eb2fbeff3f6de542
Luca Isidoro <apprendista44@gmail.com>
2020-07-27 15:18:07 +0200

    commit con elementi base

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hotel</title>
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+
+    <header>
+      <h1>BOOLEAN HOTEL</h1>
+    </header>
+
+    <main>
+      <table class="room-list">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Numero Stanze</th>
+            <th>Piano</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>5</td>
+            <td>555</td>
+            <td>50</td>
+
+          </tr>
+        </tbody>
+      </table>
+    </main>
+
+  </body>
+</html>
